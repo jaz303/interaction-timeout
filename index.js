e8ef008345d08af781de3ba4dc09a2ed7622cb36
Jason Frame <jason@onehackoranother.com>
2015-02-16 12:57:57 +0000

    tabs -> spaces

--- index.js
+++ index.js
@@ -1,53 +1,53 @@
 module.exports = function(fn, timeInMilliseconds) {
 
-	var state = 'idle';
-	var timer = null;
-	
-	function handleInteraction() {
-		if (state !== 'idle') {
-			activate();
-		}
-	}
+    var state = 'idle';
+    var timer = null;
+    
+    function handleInteraction() {
+        if (state !== 'idle') {
+            activate();
+        }
+    }
 
-	document.body.addEventListener('mousedown', handleInteraction, true);
-	document.body.addEventListener('touchstart', handleInteraction, true);
+    document.body.addEventListener('mousedown', handleInteraction, true);
+    document.body.addEventListener('touchstart', handleInteraction, true);
 
-	function activate() {
-		clear();
-		state = 'active';
-		timer = setTimeout(handleTimeout, timeInMilliseconds);
-	}
+    function activate() {
+        clear();
+        state = 'active';
+        timer = setTimeout(handleTimeout, timeInMilliseconds);
+    }
 
-	function handleTimeout() {
-		state = 'idle';
-		timer = null;
-		fn();
-	}
+    function handleTimeout() {
+        state = 'idle';
+        timer = null;
+        fn();
+    }
 
-	function clear() {
-		if (timer) {
-			clearTimeout(timer);
-			timer = null;
-		}
-	}
+    function clear() {
+        if (timer) {
+            clearTimeout(timer);
+            timer = null;
+        }
+    }
 
-	function startOnInteraction() {
-		clear();
-		state = 'wait';
-	}
+    function startOnInteraction() {
+        clear();
+        state = 'wait';
+    }
 
-	return {
-		stop: function() {
-			clear();
-			state = 'idle';
-		},
-		start: function() {
-			activate();
-		},
-		startOnInteraction: function() {
-			clear();
-			state = 'wait';
-		}
-	};
+    return {
+        stop: function() {
+            clear();
+            state = 'idle';
+        },
+        start: function() {
+            activate();
+        },
+        startOnInteraction: function() {
+            clear();
+            state = 'wait';
+        }
+    };
 
 };
